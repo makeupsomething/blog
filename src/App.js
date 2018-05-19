3a1d94a03d2081d656374a880e3b3fad6c68259c
daryl <darylcoleoffice@gmail.com>
2018-05-19 19:32:13 +0900

    add scaffold

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component, createElement } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import marksy from 'marksy';
 import logo from './logo.svg';
 import './App.css';
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const compile = marksy({
+  createElement,
+  
+  elements: {
+    h1 ({id, children}) {
+      return <Title>{children}</Title>
+    },
+  }
+});
+
 class App extends Component {
+  state = {
+    compiled_md: null,
+  }
+
+  componentDidMount = () => {
+    const testFile = require("./markdown/test.md");
+    fetch(testFile).then(response => {
+      response.text().then(text => {
+        const compiled = compile(text);
+        this.setState({compiled_md: compile(text).tree})
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router basename="/blog">
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+
+            <hr/>
+            {this.state.compiled_md && this.state.compiled_md.length > 0 ? <div>{this.state.compiled_md}</div> : null}
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/markdown/test.md
@@ -0,0 +1,8 @@
+# this is a test
+
+I think this is a test file, let's see
+
+`yay`
+
+### everything is awesome
+
