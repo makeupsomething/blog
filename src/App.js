9ce81f455c934e28c89ad908141161a11744a681
daryl <darylcoleoffice@gmail.com>
2018-05-20 23:00:19 +0900

    Add nav bar

--- src/App.js
+++ src/App.js
@@ -31,10 +31,41 @@ const BannerTitle = styled.h1`
 	font-family: 'Bree Serif', serif;
 `
 
+const NavBar = styled.ul`
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    background-color: #4844a3;
+	height: 5%;
+	width: 100%;
+    position: fixed;
+	overflow: auto;
+`
+
+const NavContents = styled.li`
+	display: inline-block;
+	height: 100%;
+	width: 20%;
+`
+
+const NavButton = styled.div`
+	height: 100%;
+	font-family: 'Lora', serif;
+	font-size: 1.0em;
+	text-align: center;
+	text-decoration: none;
+	border-left: solid 2px #6b6f83;
+
+	${NavButton}:hover {
+		background: #6b6f83;
+	}
+`
+
 const Line = styled.hr`
 	background-color: #7aacd4;
 	height: 2px;
 	margin: 0px;
+	border: none;
 `;
 
 class App extends Component {
@@ -55,13 +86,20 @@ class App extends Component {
 						<BannerTitle>{`makeupsomethinglater's \n blog`}</BannerTitle>
 					</BannerWrapper>
 					<Line />
-					<ul>
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/about">About</Link></li>
-						{this.state.posts.map((post, index) => {
-							return <li><Link to={`/posts/${index}`}>{post.title}</Link></li>
-						})}
-					</ul>
+					<NavBar>
+						<NavContents>
+							<Link to="/"><NavButton>Home</NavButton></Link>
+						</NavContents>
+						<NavContents>
+							<Link to="/resume"><NavButton>Resume</NavButton></Link>
+						</NavContents>
+						<NavContents>
+							<Link to="/works"><NavButton>What Im Working on</NavButton></Link>
+						</NavContents>
+					</NavBar>
+					{this.state.posts.map((post, index) => {
+							return <div><Link to={`/posts/${index}`}>{post.title}</Link></div>
+					})}
 					<Route exact path="/" component={Home}/>
 					<Route path="/about" component={About}/>
 					<Route path={`/posts/:postId`} component={Post}/>
