ffb9a1890efa7a2fc1ad33564d3027c4b820df25
daryl <darylcoleoffice@gmail.com>
2018-09-09 22:30:52 +0900

    update post preview title width

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 const Title = styled.h1`
   	font-size: 2.7em;
+    width: 90%;
     color: #4844a3;
     font-family: 'Bree Serif', serif;
     text-decoration: underline;
