c9e98dc7494c386ebc8a7f33af18f822ebbf3c7c
daryl <darylcoleoffice@gmail.com>
2018-12-27 18:20:37 +0900

    Add social links to about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Wrapper, SectionTitle } from './styled/Section'
+import { SectionTitle } from './styled/Section'
 
 const Parent = styled.div`
 	height: 75vh;
@@ -17,6 +17,17 @@ const Paragraph = styled.p`
     font-family: 'Bree Serif', serif;
 `;
 
+const SocialSection = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+const Link = styled.a`
+    color: white;
+    cursor: pointer;
+`
+
 export default function About() {
     return (
         <Parent>
@@ -25,6 +36,17 @@ export default function About() {
                 Thank you for checking out my site!
                 I am a web developer in Tokyo, Japan
             </Paragraph>
+            <SocialSection>
+                <Link href="https://github.com/makeupsomething">
+                    <ion-icon name="logo-github" size="large"></ion-icon>
+                </Link>
+                <Link href="https://www.linkedin.com/in/daryl-cole/">
+                    <ion-icon name="logo-linkedin" size="large"></ion-icon>
+                </Link>
+                <Link href="https://twitter.com/mkupsmth">
+                    <ion-icon name="logo-twitter" size="large"></ion-icon>
+                </Link>
+            </SocialSection>
         </Parent>
     )
 }
